035be69e67464f645feeed5275dfe63127b6c3dc
jooms6992 <70081544+jooms6992@users.noreply.github.com>
2023-06-01 12:13:43 +0900

    Time: 65 ms (32.94%), Space: 42.3 MB (67.06%) - LeetHub

--- LeetCode/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ LeetCode/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,6 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
+    nums.reduce((a,c,i,arr) => arr[i] += a)
+    return nums
     let sum = 0;
     const result = []
     for(let i = 0; i < nums.length; i++) {
